d7a36a2235ea003f8dcb5d18bf4a0369d8d2070b
Daniel Zello <75264908+Ravost99@users.noreply.github.com>
2022-04-12 17:35:03 +0000

    automatic updates v2

--- auto_update.py
+++ auto_update.py
@@ -1,7 +1,7 @@
 import urllib, colors, os
 
 def update():
-  stuff_to_update = ['main.py', 'colors.py', 'templates/index.html', 'README.md', 'setup.sh', 'auto_update']
+  stuff_to_update = ['main.py', 'colors.py', 'templates/index.html', 'README.md', 'setup.sh', 'auto_update.py']
   for item in stuff_to_update:
     dat = urllib.request.urlopen("https://raw.githubusercontent.com/Ravost99/ping/master/" + item).read()
     with open(item, 'wb') as file:
