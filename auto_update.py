8bce2fae65406f57a31aa1a8f5b343eef61b55c3
Daniel Zello <75264908+Ravost99@users.noreply.github.com>
2022-04-18 19:49:10 +0000

    automatic updates ++

--- auto_update.py
+++ auto_update.py
@@ -4,7 +4,12 @@ def update():
   stuff_to_update = ['main.py', 'colors.py', 'templates/index.html', 'README.md', 'setup.py', 'auto_update.py']
   for item in stuff_to_update:
     dat = urllib.request.urlopen("https://raw.githubusercontent.com/Ravost99/ping/master/" + item).read()
-    with open(item, 'wb') as file:
-      file.write(dat)
-  print(f'{colors.green}Updated Successfull!')
+    update = input(f"There is a new update in {item}, would you like to update? (Will override {colors.underline}everything{colors.reset} in {item}) (Y/N) ")
+    if update.lower() == 'y':
+      with open(item, 'wb') as file:
+        file.write(dat)
+      print(f'{colors.green}Updated Successfull!')
+    else:
+      print(f"{colors.dark_red}Cancled Update in {item}")
+  print("Restarting...")
   os.system('python3 main.py')
