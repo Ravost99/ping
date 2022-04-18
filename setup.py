ee7f966216860570a63ae4de7579361366fdbe8a
Daniel Zello <75264908+Ravost99@users.noreply.github.com>
2022-04-18 14:20:21 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 import os, time
 
-logging = input("Do you want logging?")
+logging = input("Do you want logging? (Y/N)\n")
+rounds = input("Do you want ping rounds (Y/N)\n")
+if logging.lower() == 'y':
+  logging = True
+else:
+  logging = False
+
+if rounds.lower() == 'y':
+  rounds = True
+else:
+  rounds = False
 
 def check(file):
   if os.path.isfile(file) == True:
@@ -15,5 +25,7 @@ check('sites.txt')
 print("Setup Completed!")
 print("Restarting with main.py")
 time.sleep(2)
+with open('config.py', 'a') as f:
+  f.write(f'setup = True\nlogging = {logging}\nping_rounds = {rounds}')
 os.remove(__file__)
 os.system('python3 main.py')
