e642d7d8ec6b469346210482ea45316ffd66fdd6
Daniel Zello <75264908+Ravost99@users.noreply.github.com>
2022-04-18 19:43:36 +0000

    import errors

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import requests, time, colors, os, time, pytz, config
+try:
+  import requests, time, colors, os, time, pytz, config
+except:
+  import os
+  os.system('pip install requests pytz')
+  os.system('pip install -U pip')
+
 from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 from auto_update import update
